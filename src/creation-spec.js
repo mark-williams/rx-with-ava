25522245873e458de873ceb399e07eb5fdc11cad
mark-williams <markwill@hotmail.co.uk>
2017-04-02 10:51:17 +0100

    Added test for subscription complete method called

--- src/creation-spec.js
+++ src/creation-spec.js
@@ -1,14 +1,21 @@
 import test from 'ava';
+import sinon from 'sinon';
+
 import createObservable from './creation';
 
-test('observable creation from array', t => {
-  const testArray = [1, 3, 7, 11];
-  const results = [];
+const testArray = [1, 3, 7, 11];
+const results = [];
 
+test('observable creation from array', t => {
+  const complete = sinon.spy();
   const source = createObservable(testArray);
-  source.subscribe(x => {
-    results.push(x);
-  });
+  source.subscribe(
+    x => {
+      results.push(x);
+    },
+    () => {},
+    complete);
 
   t.deepEqual(results, testArray);
+  t.truthy(complete.calledOnce);
 });
