ae4075d13aae96e26cd6ea6ee15ab47ac60aeff8
mark-williams <markwill@hotmail.co.uk>
2017-04-02 10:55:35 +0100

    Refactored tests

--- src/creation-spec.js
+++ src/creation-spec.js
@@ -4,18 +4,25 @@ import sinon from 'sinon';
 import createObservable from './creation';
 
 const testArray = [1, 3, 7, 11];
-const results = [];
 
 test('observable creation from array', t => {
-  const complete = sinon.spy();
+  const results = [];
   const source = createObservable(testArray);
   source.subscribe(
     x => {
       results.push(x);
-    },
+    });
+
+  t.deepEqual(results, testArray);
+});
+
+test('complete method called', t => {
+  const complete = sinon.spy();
+  const source = createObservable(testArray);
+  source.subscribe(
+    () => {},
     () => {},
     complete);
 
-  t.deepEqual(results, testArray);
   t.truthy(complete.calledOnce);
 });
