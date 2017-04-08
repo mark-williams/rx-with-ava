260afbd63f825e6446b73bb7d2ef824e5137149d
mark-williams <markwill@hotmail.co.uk>
2017-04-08 17:00:20 +0100

    Subscribe to mouse events

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-import mouseMoves from './src/events/mouse';
+import mouseMove from './src/events/mouse';
+import { Observable } from 'rxjs';
 
-mouseMoves.subscribe(m => {
-  console.log(m);
-});
+const circle = document.getElementsByClassName('circle')[0];
+mouseMove(document)
+  .filter(e => {
+    return (e.clientX > 20 && e.clientX < (window.innerWidth - 40)) &&
+      (e.clientY > 20 && e.clientY < (window.innerHeight - 40));
+  })
+  .map(e => {
+    return { x: e.clientX, y: e.clientY };
+  })
+  .delay(200)
+  .subscribe(m => {
+    circle.style.left = m.x;
+    circle.style.top = m.y;
+  });
