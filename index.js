d3782889052557439a8b378cdbf4205e781bd8fe
mark-williams <markwill@hotmail.co.uk>
2017-04-09 09:08:23 +0100

    Set up simple controls to demo fetching as observables

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 import mouseMove from './src/events/mouse';
 import { Observable } from 'rxjs';
 
-const circle = document.getElementsByClassName('circle')[0];
-mouseMove(document)
+const moreButton = document.getElementById('more');
+const moreClick = Observable.fromEvent(moreButton, 'click');
+const clearButton = document.getElementById('clear');
+const clearClick = Observable.fromEvent(clearButton, 'click');
+const resultsTarget = document.getElementById('results');
+
+moreClick.subscribe(() => {
+  appendData('wwww');
+});
+
+clearClick.subscribe(() => {
+  resultsTarget.innerText = '';
+});
+
+
+const appendData = (data) => {
+  const result = document.createElement('div');
+  result.innerText = data;
+
+  resultsTarget.appendChild(result);
+};
+
+
+const circles = document.getElementsByClassName('circle');
+if (circles.length) {
+  const circle = circles[0];
+  mouseMove(document)
   .filter(e => {
     return (e.clientX > 20 && e.clientX < (window.innerWidth - 40)) &&
       (e.clientY > 20 && e.clientY < (window.innerHeight - 40));
@@ -15,3 +40,4 @@ mouseMove(document)
     circle.style.left = m.x;
     circle.style.top = m.y;
   });
+}
